d02c2bc25233166d0edc614358df15ffd10a120c
Moor, Sergej <sergej@exord.de>
2023-06-26 18:34:24 +0200

    es linting update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: "@typescript-eslint/parser"
   },
   extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
   plugins: ["prettier"],
-  rules: { "prettier/prettier": ["error", { endOfLine: "off" }] },
+  rules: { "prettier/prettier": ["error", { endOfLine: "crlf" }] }
 };

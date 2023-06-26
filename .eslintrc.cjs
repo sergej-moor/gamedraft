5ced3a459c011f61bf1ed2f12aec5e04bc7191f3
Sergej Moor <sergej.moor@stud.th-luebeck.de>
2023-06-26 03:52:59 +0200

    eslint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,5 @@ module.exports = {
   },
   extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
   plugins: ["prettier"],
-  rules: {},
+  rules: { "prettier/prettier": ["error", { endOfLine: "off" }] },
 };
